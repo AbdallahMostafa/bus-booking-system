1c34e5e7b87d0c83ae9cf46b22dcb643ff7c719e
AbdallahMostafa <abdallahmostafa377@gmail.com>
2023-09-26 23:02:49 +0300

    Form request to handle the inputs of the user when registering

---
+++ app/Http/Requests/CreateUserRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class CreateUserRequest extends FormRequest
+{
+  
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors();
+
+        throw new HttpResponseException(response()->json(['errors' => $errors], 422));
+    }
+}
