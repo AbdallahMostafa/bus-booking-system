79fadae5099ea9daf3214f18182db6855506230a
AbdallahMostafa <abdallahmostafa377@gmail.com>
2023-09-28 02:55:21 +0300

    migration file for the stations table with the trip relation

---
+++ database/migrations/2023_09_27_181402_create_stations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stations', function (Blueprint $table) {
+            $table->id();
+            $table->string('Name');
+            $table->integer('order_in_trip');
+            
+            $table->unsignedBigInteger('trip_id');
+            $table->foreign('trip_id')->references('id')->on('trips'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stations');
+    }
+};
