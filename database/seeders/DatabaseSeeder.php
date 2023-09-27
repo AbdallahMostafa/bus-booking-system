99d8df8e45b8056685adccf77cbb483d8da37af5
AbdallahMostafa <abdallahmostafa377@gmail.com>
2023-09-28 02:57:49 +0300

    modified the database seeder to run all the seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory(15)->create();
+        $this->call(BusesSeeder::class);
+        $this->call(SeatsSeeder::class);
+        $this->call(TripsSeeder::class);
+        $this->call(StationsSeeder::class);
+        $this->call(ReservationsSeeder::class);
+        
     }
 }
